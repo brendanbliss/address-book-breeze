95923cf59fd9dec4dee3bdb5bf5db6cc6cb52d1f
Brendan Bliss <brendanmbliss@gmail.com>
2021-02-19 11:50:35 -0700

    added url to hosts list

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,5 +8,6 @@ end
 Rails.application.config.hosts = [
   IPAddr.new("0.0.0.0/0"), # All IPv4 addresses.
   IPAddr.new("::/0"),      # All IPv6 addresses.
-  "localhost"              # The localhost reserved domain.
+  "localhost",              # The localhost reserved domain.
+  "addressbook-breeze.herokuapp.com"
 ]
